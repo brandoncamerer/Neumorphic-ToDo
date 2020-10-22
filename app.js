845b4e88c3c8091acc2462774c6d1a743e44f1e1
brandoncamerer <50874245+brandoncamerer@users.noreply.github.com>
2020-10-22 10:14:19 -0700

    test

--- app.js
+++ app.js
@@ -2,73 +2,51 @@ let addToDoButton = document.getElementById("addToDo");
 let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
 
-
-
+// Testing Committ
 //Create todo list item
-  addToDoButton.addEventListener("click", function () {
-    var li = document.createElement("Li");
-    var divRight = document.createElement("div");
-    var divLeft = document.createElement("div");
-    var radio = document.createElement("div");
-    var txt = document.createElement("span");
-    var input = document.getElementById('inputField').value.trim();
+addToDoButton.addEventListener("click", function () {
+  var li = document.createElement("Li");
+  var divRight = document.createElement("div");
+  var divLeft = document.createElement("div");
+  var radio = document.createElement("div");
+  var txt = document.createElement("span");
+  var input = document.getElementById("inputField").value.trim();
 
-    if (!input.length) return false;  // cancel click
-
-else { 
-
-
+  if (!input.length) return false;
+  // cancel click
+  else {
     txt.innerText = inputField.value;
     radio.classList.add("radio");
     divRight.classList.add("trash");
     divLeft.appendChild(radio);
     divLeft.appendChild(txt);
-   
-  
-   
 
     li.appendChild(divLeft);
     li.appendChild(divRight);
- 
+
     toDoContainer.appendChild(li);
     inputField.value = "";
-
   }
 
-    
   divLeft.addEventListener("click", function () {
-
     var x = radio.classList.contains("completed");
-  
 
-    if (x === true){
-
-      radio.classList.remove('completed');
-      radio.classList.add('radio');
+    if (x === true) {
+      radio.classList.remove("completed");
+      radio.classList.add("radio");
       txt.style.opacity = "100%";
       li.style.opacity = "100%";
       txt.style.textDecoration = "none";
-
+    } else {
+      radio.classList.remove("radio");
+      radio.classList.add("completed");
+      txt.style.textDecoration = "line-through";
+      txt.style.opacity = "50%";
+      li.style.opacity = "50%";
     }
-
-    else {
-
-    radio.classList.remove('radio');
-    radio.classList.add('completed');
-    txt.style.textDecoration = "line-through";
-    txt.style.opacity = "50%";
-    li.style.opacity = "50%";
-
-  }
-  
-   
-
-    
   });
 
   divRight.addEventListener("click", function () {
-  toDoContainer.removeChild(li);
-    });
-
+    toDoContainer.removeChild(li);
+  });
 });
-
